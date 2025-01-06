3b613392b5cc90a8e643ce269faa4be2902b36fa
Luke Slater <lslater@3G1.42angouleme.fr>
2025-01-06 17:31:22 +0100

    added the collision limits in the bedroom scene , started adding the collisions for the bed and portal , need a temporary character sprite to play and debug with and start setting up the different interactions (portal  = changing scene / bed change time / computer open computer screen etc etc)

---
+++ assets/scenes/bedroom.gd
@@ -0,0 +1,5 @@
+extends Node2D
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if(Input.is_action_pressed("ui_cancel")):
+		get_tree().quit()

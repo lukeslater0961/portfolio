6fb66005dc37870b920a29338cb5199daea31111
Luke Slater <lslater@3G7.42angouleme.fr>
2025-01-07 16:32:19 +0100

    added fuck as movement to etst some collisions , added the bed collisions and a sprite to make the character appear behind

--- assets/scenes/character_body_2d.gd
+++ assets/scenes/character_body_2d.gd
@@ -1,16 +1,16 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
-
 func _physics_process(delta: float) -> void:
-
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var input_direction = Vector2(
+		Input.get_axis("ui_left", "ui_right"),  # Horizontal movement
+		Input.get_axis("ui_up", "ui_down")     # Vertical movement
+	)
+	if input_direction != Vector2.ZERO:
+		velocity = input_direction.normalized() * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity = Vector2.ZERO
 
 	move_and_slide()

b3fd37adfe49f45ef0663a249775a13fa3812bac
Luke Slater <luke.a.slater@outlook.com>
2025-01-15 17:42:46 +0100

    added fake dialog box for e on bed and trying to apply transition , need to look at how transitions are done

---
+++ dialog_box.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@onready var box: Sprite2D = $"../Box"
+@onready var player: Node2D = $Player
+
+var isentered : bool  = false
+
+func _ready() -> void:
+	$"../Box".visible = false
+
+func _process(delta: float) -> void:
+	if isentered and Input.is_key_pressed(KEY_E):
+		Globals.DoSleepCycle()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		print_debug("showing")
+		$"../Box".visible = true
+		isentered = true
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		print_debug("hiding")
+		$"../Box".visible = false
+		isentered = false
--- scripts/bedroom.gd
+++ scripts/bedroom.gd
@@ -1,4 +1,10 @@
 extends Node2D
+@onready var fade_transition: ColorRect = $FadeTransition
+
+func _ready() -> void:
+	$FadeTransition.hide
+	print_debug("hid transition")
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if(Input.is_action_pressed("ui_cancel")):

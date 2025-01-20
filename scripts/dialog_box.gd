3f848215cf0db61c9d47291dce04238b0c03d7bf
Luke Slater <luke.a.slater@outlook.com>
2025-01-20 12:58:45 +0100

    modified interact script to handle multiple objects in the scene (more modular)

--- scripts/dialog_box.gd
+++ scripts/dialog_box.gd
@@ -1,19 +1,16 @@
 extends Node2D
 
-@onready var label: Label = $"../Label"
 
-
-var ObjectType: String 
+@onready var label: Label = $Label
 var isentered : bool  = false
 
 func _ready() -> void:
-	ObjectType = str(get_parent().name)
-	print(ObjectType)
 	label.visible = false
 
 func _process(_delta: float) -> void:
 	if isentered and Input.is_key_pressed(KEY_E):
-		doInteract()
+		Globals.DoSleepCycle()
+		isentered = false 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
@@ -26,11 +23,3 @@ func _on_body_exited(body: Node2D) -> void:
 		print_debug("hiding")
 		label.visible = false
 		isentered = false
-
-func doInteract() -> void:
-	if (ObjectType == "Bed"):
-		Globals.DoSleepCycle()
-		isentered = false 
-	else: if (ObjectType == "Portal"):
-		print("boo")
-		isentered = false 
